a111eb9f43aa729a86f9fef0c22049f0c7462efd
Jefferson Alves <jeffersonalmeida16@outlook.com>
2021-11-15 22:41:01 -0300

    add resource server

--- src/main/java/com/example/food/infrastructures/security/ResourceServerConfig.java
+++ src/main/java/com/example/food/infrastructures/security/ResourceServerConfig.java
@@ -1,38 +1,28 @@
 package com.example.food.infrastructures.security;
 
-import org.hibernate.stat.internal.SessionStatisticsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final String KITCHEN_URL = "/kitchen/**";
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
-                .and()
-                    .authorizeRequests()
-                    .antMatchers(KITCHEN_URL)
-                        .permitAll()
+        http.authorizeRequests()
                     .anyRequest()
                     .authenticated()
                 .and()
                     .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                    .csrf()
-                    .disable();
+                    .oauth2ResourceServer()
+                    .opaqueToken();
     }
 
 }
-clear
-
-casl
-
-
